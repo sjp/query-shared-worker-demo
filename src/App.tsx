5495216234cceb9febfd62c8eda2e98282cb8e4b
Simon Potter <simon@sjp.co.nz>
2025-03-18 00:13:34 +0000

    Prefer persistQueryClient to the specialised context
    Also minor config tweak to quickly run/test via devcontainers

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,8 @@
 import { BrowserRouter as Router } from "react-router-dom";
 import { ThemeProvider } from "@mui/material";
 import { createTheme } from "@mui/material/styles";
-import { OmitKeyof, QueryClient } from "@tanstack/react-query";
-import {
-  PersistQueryClientOptions,
-  PersistQueryClientProvider,
-} from "@tanstack/react-query-persist-client";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { persistQueryClient } from "@tanstack/react-query-persist-client";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import Layout from "./Layout";
 import { broadcastQueryClient } from "@tanstack/query-broadcast-client-experimental";
@@ -30,28 +27,29 @@ const queryClient = new QueryClient({
   },
 });
 
+// enable updates to be shared across tabs
 broadcastQueryClient({
   queryClient,
 });
 
 const sharedWorkerPersister = createSharedWorkerPersister();
 
-const persistOptions: OmitKeyof<PersistQueryClientOptions, "queryClient"> = {
-  persister: sharedWorkerPersister,
-};
+// enable fresh tabs to load the shared query cache
+// while still receiving updates via the broadcastQueryClient
+persistQueryClient({
+  queryClient,
+  persister: sharedWorkerPersister
+});
 
 export default function App() {
   return (
-    <PersistQueryClientProvider
-      client={queryClient}
-      persistOptions={persistOptions}
-    >
+    <QueryClientProvider client={queryClient}>
       <Router>
         <ThemeProvider theme={theme}>
           <Layout />
           <ReactQueryDevtools initialIsOpen />
         </ThemeProvider>
       </Router>
-    </PersistQueryClientProvider>
+    </QueryClientProvider>
   );
 }
